5e25f227d942fde808ff99a9ae01d131a085718d
Jahi Stewart <jahikstewart@lewisu.edu>
2021-09-27 05:56:02 -0500

    Updates

--- dice_roller.js
+++ dice_roller.js
@@ -1,10 +1,17 @@
 function rollSixDice() {
+    var total=0;
     for(let i=1; i<7; i++){
-        document.getElementById("die"+i).value = Math.floor(Math.random()*6) + 1;
+        var num=Math.floor(Math.random()*6) + 1
+        document.getElementById("die"+i).value = num;
+        total+=num
     } 
+    document.getElementById("total").value= total;
 }
 function displayDice(){
+    //Display Dice
     for(let i = 1; i < 7; i++) {
-        document.body.innerHTML +='Die #'+i+': <input id="die'+i+'" type="text" value="" size="5" readonly><br><br>'
+        document.body.innerHTML +='<span>Die #'+i+': <input id="die'+i+'" type="text" value="" size="5" readonly></span>'
      }
+    //Display Total
+    document.body.innerHTML +='<br><br>Total: <input id="total" type="text" value="" size="5" readonly><br>'
 }
